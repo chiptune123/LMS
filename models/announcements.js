383a0a412a983c955f101e9524f59d3bf732fa99
Chiptune <phamtienphat123@gmail.com>
2024-01-04 23:17:37 -0800

    Implement announcements model

---
+++ models/announcements.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AnnouncementSchema = new Schema({
+    creationDate: {typeof: Date, required: true},
+    announcementContent: {typeof: Date, required: true},
+    writeID: {typeof: Schema.Types.ObjectId, required: true }
+})
+
+module.exports = mongoose.model("Announcement", AnnouncementSchema);
