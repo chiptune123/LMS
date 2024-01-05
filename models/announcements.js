08770a4259f1653492e08f5ddc3719c7bb33e400
Chiptune <phamtienphat123@gmail.com>
2024-01-05 00:29:05 -0800

    Fix syntax error due to forget reference

--- models/announcements.js
+++ models/announcements.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const AnnouncementSchema = new Schema({
     creationDate: {typeof: Date, required: true},
     announcementContent: {typeof: Date, required: true},
-    writeID: {typeof: Schema.Types.ObjectId, required: true }
+    writeID: {typeof: Schema.Types.ObjectId, ref:"User", required: true }
 })
 
 module.exports = mongoose.model("Announcement", AnnouncementSchema);
