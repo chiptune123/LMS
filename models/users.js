e61b0359e1d6e2142a2838be6e622b8a6083a6df
Chiptune <phamtienphat123@gmail.com>
2024-01-05 21:03:04 -0800

    Fix syntax error of type

--- models/users.js
+++ models/users.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { typeof: String, required: true, maxLength: 100 },
-    password: { typeof: String, required: true, maxLength: 100 },
-    name: { typeof: String, required: true, maxLength: 100 },
-    email: { typeof: String, required: true, maxLength: 100 },
-    phoneNumber: {typeof: Number},
-    address: {typeof: String, required: true, maxLength: 100},
-    role: {typeof: String, required: true, maxLength: 100},
-    verificationStatus: {typeof: Boolean, default: 0},
-    profilePicture: {typeof: String, maxLength: 500},
-    deleteStatus: {typeof: Boolean, default: 0},
-    deleteReason: {typeof: String, maxLength: 500},
+    username: { type: String, required: true, maxLength: 100 },
+    password: { type: String, required: true, maxLength: 100 },
+    name: { type: String, required: true, maxLength: 100 },
+    email: { type: String, required: true, maxLength: 100 },
+    phoneNumber: {type: Number},
+    address: {type: String, required: true, maxLength: 100},
+    role: {type: String, required: true, maxLength: 100},
+    verificationStatus: {type: Boolean, default: 0},
+    profilePicture: {type: String, maxLength: 500},
+    deleteStatus: {type: Boolean, default: 0},
+    deleteReason: {type: String, maxLength: 500},
 })
 
 module.exports = mongoose.model("User", UserSchema);
