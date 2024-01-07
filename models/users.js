b0abb5fbaa4316f81fc11f7658c3e9704ec23083
Chiptune <phamtienphat123@gmail.com>
2024-01-06 16:34:19 -0800

    Add default value for trivial field and remove required constraint

--- models/users.js
+++ models/users.js
@@ -7,13 +7,13 @@ const UserSchema = new Schema({
     password: { type: String, required: true, maxLength: 100 },
     name: { type: String, required: true, maxLength: 100 },
     email: { type: String, required: true, maxLength: 100 },
-    phoneNumber: {type: Number},
-    address: {type: String, required: true, maxLength: 100},
-    role: {type: String, required: true, maxLength: 100},
-    verificationStatus: {type: Boolean, default: 0},
-    profilePicture: {type: String, maxLength: 500},
-    deleteStatus: {type: Boolean, default: 0},
-    deleteReason: {type: String, maxLength: 500},
+    phoneNumber: {type: Number, default: ""},
+    address: {type: String, maxLength: 100, default: ""},
+    role: {type: String, maxLength: 100, enum: ["Admin", "User", "Librarian"], default: "User"},
+    verificationStatus: {type: Boolean, default: false},
+    profilePicture: {type: String, maxLength: 500, default: ""},
+    deleteStatus: {type: Boolean, default: false},
+    deleteReason: {type: String, maxLength: 500, default: ""},
 })
 
 module.exports = mongoose.model("User", UserSchema);
