1976b56afaedb2c3a0aab50d7a124791af99da53
Chiptune <phamtienphat123@gmail.com>
2024-01-09 01:02:47 -0800

    Implement view and controller to show announcement list

---
+++ controllers/announcementController.js
@@ -0,0 +1,39 @@
+const Announcement = require('../models/announcements');
+const asyncHandler = require('express-async-handler');
+const { body, validatorResult } = require('express-validator');
+
+exports.announcement_list = asyncHandler(async (req, res, next) => {
+    const announcementList =  await Announcement.find({})
+        .sort({ creationDate: 1 })
+        .exec();
+    console.log(announcementList);
+    res.render("announcement_list", { title: "Announcement List", announcement_list: announcementList });
+})
+
+exports.announcement_create_get = asyncHandler(async (req,res,next) =>{
+    res.send("Not implement create get");
+})
+
+exports.announcement_create_post = asyncHandler(async(req,res,next) =>{
+    res.send("Not implement create post");
+})
+
+exports.announcement_detail = asyncHandler(async(req,res,next) =>{
+    res.send("Not implement announcement detail");
+})
+
+exports.announcement_delete_get = asyncHandler(async(req,res,next) =>{
+    res.send("Not implement announcement delete get");
+})
+
+exports.announcement_delete_post = asyncHandler(async(req,res,next) =>{
+    res.send("Not implement announcement delete post");
+})
+
+exports.announcement_update_get = asyncHandler(async(req,res,next) =>{
+    res.send("Not implement annoucement update get");
+})
+
+exports.announcement_update_post = asyncHandler(async(req,res,next) => {
+    res.send("Not implement announcement update post")
+})
