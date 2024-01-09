a80978d51e282429b9daa85d3151aaae2274e998
Chiptune <phamtienphat123@gmail.com>
2024-01-09 01:22:51 -0800

    Require userID before allow to create announcement

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -6,9 +6,9 @@ const annoucement_controller = require('../controllers/announcementController');
 
 router.get("/", annoucement_controller.announcement_list);
 // Create announcement routes
-router.get("/create", annoucement_controller.announcement_create_get);
+router.get("/:id/create", annoucement_controller.announcement_create_get);
 
-router.post("/create", annoucement_controller.announcement_create_post);
+router.post("/:id/create", annoucement_controller.announcement_create_post);
 // Announcement detail routes
 router.get("/:id", annoucement_controller.announcement_detail);
 // Announcement delete routes
