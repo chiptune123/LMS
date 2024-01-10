5f1d32c12105854129e9188bc446dabbf0a9f153
Chiptune <phamtienphat123@gmail.com>
2024-01-10 12:17:45 -0800

    Fix grammar error

--- models/announcements.js
+++ models/announcements.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const AnnouncementSchema = new Schema({
     creationDate: {type: Date},
     announcementContent: {type: String, required: true},
-    writeID: {type: String, required: true }
+    writerID: {type: String, required: true }
 })
 
 module.exports = mongoose.model("Announcement", AnnouncementSchema);
