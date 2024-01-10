89d13e2a6398d5e540af551c5b92299f63b8f237
Chiptune <phamtienphat123@gmail.com>
2024-01-10 12:49:44 -0800

    Implement announcement detail controller

--- controllers/announcementController.js
+++ controllers/announcementController.js
@@ -1,48 +1,55 @@
-const Announcement = require('../models/announcements');
-const asyncHandler = require('express-async-handler');
-const { body, validatorResult } = require('express-validator');
+const Announcement = require("../models/announcements");
+const asyncHandler = require("express-async-handler");
+const { body, validatorResult } = require("express-validator");
 
 exports.announcement_list = asyncHandler(async (req, res, next) => {
-    const announcementList =  await Announcement.find({})
-        .sort({ creationDate: 1 })
-        .exec();
-    console.log(announcementList);
-    res.render("announcement_list", { title: "Announcement List", announcement_list: announcementList });
-})
+  const announcementList = await Announcement.find({})
+    .sort({ creationDate: 1 })
+    .exec();
+  console.log(announcementList);
+  res.render("announcement_list", {
+    title: "Announcement List",
+    announcement_list: announcementList,
+  });
+});
 
-exports.announcement_create_get = asyncHandler(async (req,res,next) =>{
-    res.render("announcement_create_form", {title:"Create Announcement"});
-})
+exports.announcement_create_get = asyncHandler(async (req, res, next) => {
+  res.render("announcement_create_form", { title: "Create Announcement" });
+});
 
-exports.announcement_create_post = asyncHandler(async(req,res,next) =>{
-    const currentDate = new Date();
-    const NewAnnouncement = new Announcement({
-        creationDate: currentDate,
-        announcementContent: req.body.announcementContent,
-        writerID: req.params.id
-    }) 
+exports.announcement_create_post = asyncHandler(async (req, res, next) => {
+  const currentDate = new Date();
+  const NewAnnouncement = new Announcement({
+    creationDate: currentDate,
+    announcementContent: req.body.announcementContent,
+    writerID: req.params.id,
+  });
 
-    await NewAnnouncement.save();
-    res.redirect('/announcements');
-})
+  await NewAnnouncement.save();
+  res.redirect("/announcements");
+});
 
-exports.announcement_detail = asyncHandler(async(req,res,next) =>{
-  const announcementDetail = Announcement.findById(req.params.id).exec();
-  res.render("announcement_detail", {title: "Announcement Detail", announcement_detail: announcementDetail});
-})
+exports.announcement_detail = asyncHandler(async (req, res, next) => {
+  const announcementDetail = await Announcement.findById(req.params.id).exec();
+  console.log(announcementDetail);
+  res.render("announcement_detail", {
+    title: "Announcement Detail",
+    announcement_detail: announcementDetail,
+  });
+});
 
-exports.announcement_delete_get = asyncHandler(async(req,res,next) =>{
-    res.send("Not implement announcement delete get");
-})
+exports.announcement_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("Not implement announcement delete get");
+});
 
-exports.announcement_delete_post = asyncHandler(async(req,res,next) =>{
-    res.send("Not implement announcement delete post");
-})
+exports.announcement_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("Not implement announcement delete post");
+});
 
-exports.announcement_update_get = asyncHandler(async(req,res,next) =>{
-    res.send("Not implement annoucement update get");
-})
+exports.announcement_update_get = asyncHandler(async (req, res, next) => {
+  res.send("Not implement annoucement update get");
+});
 
-exports.announcement_update_post = asyncHandler(async(req,res,next) => {
-    res.send("Not implement announcement update post")
-})
+exports.announcement_update_post = asyncHandler(async (req, res, next) => {
+  res.send("Not implement announcement update post");
+});
