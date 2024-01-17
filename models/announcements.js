2884fdcf27dc958b4082b9dbcabd33dd1540f56a
Chiptune <phamtienphat123@gmail.com>
2024-01-17 01:21:43 -0800

    change announcement model to link to user document

--- models/announcements.js
+++ models/announcements.js
@@ -5,11 +5,15 @@ const Schema = mongoose.Schema;
 const AnnouncementSchema = new Schema({
     creationDate: {type: Date},
     announcementContent: {type: String, required: true},
+<<<<<<< HEAD
     writerID: {type: String, required: true }
 })
 
 AnnouncementSchema.virtual("getAnnouncementObjectID").get(function(){
   return `/announcements/${this.id}`;
+=======
+    writeID: {type: Schema.Types.ObjectId, ref: "User" }
+>>>>>>> 773fe3b (change announcement model to link to user document)
 })
 
 module.exports = mongoose.model("Announcement", AnnouncementSchema);
