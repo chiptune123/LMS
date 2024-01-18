d5d9cc3592c028a753aa9cfc60d25525cbb563ef
Chiptune <phamtienphat123@gmail.com>
2024-01-17 20:40:59 -0800

    Fix field name errors on modal that cause writerID couldn't save

--- models/announcements.js
+++ models/announcements.js
@@ -5,15 +5,7 @@ const Schema = mongoose.Schema;
 const AnnouncementSchema = new Schema({
     creationDate: {type: Date},
     announcementContent: {type: String, required: true},
-<<<<<<< HEAD
-    writerID: {type: String, required: true }
-})
-
-AnnouncementSchema.virtual("getAnnouncementObjectID").get(function(){
-  return `/announcements/${this.id}`;
-=======
-    writeID: {type: Schema.Types.ObjectId, ref: "User" }
->>>>>>> 773fe3b (change announcement model to link to user document)
+    writerID: {type: Schema.Types.ObjectId, ref: "User" }
 })
 
 module.exports = mongoose.model("Announcement", AnnouncementSchema);
