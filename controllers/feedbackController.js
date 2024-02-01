6b632f78956f4bc671d03eadbe8976c1cc97fd49
Chiptune <phamtienphat123@gmail.com>
2024-01-31 22:54:34 -0800

    Implement feedback list

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -1,19 +1,28 @@
-const Feedback = require("../models/feedbacks");
+const FeedbackModel = require("../models/feedbacks");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
-exports.feedback_list = asyncHandler(async (req, res, next) => {});
+exports.feedback_list = asyncHandler(async (req, res, next) => {
+    const feedbackList = await FeedbackModel
+        .find({}, "creationDate name email")
+        .sort({ creationDate: 1 })
+        .exec();
+    res.render("feedback_list", {
+        title: "Feedback list",
+        feedback_list: feedbackList
+    });
+});
 
-exports.feedback_detail = asyncHandler(async (req, res, next) => {});
+exports.feedback_detail = asyncHandler(async (req, res, next) => { });
 
-exports.feedback_create_get = asyncHandler(async (req, res, next) => {});
+exports.feedback_create_get = asyncHandler(async (req, res, next) => { });
 
-exports.feedback_create_post = asyncHandler(async (req, res, next) => {});
+exports.feedback_create_post = asyncHandler(async (req, res, next) => { });
 
-exports.feedback_update_get = asyncHandler(async (req, res, next) => {});
+exports.feedback_update_get = asyncHandler(async (req, res, next) => { });
 
-exports.feedback_update_post = asyncHandler(async (req, res, next) => {});
+exports.feedback_update_post = asyncHandler(async (req, res, next) => { });
 
-exports.feedback_delete_get = asyncHandler(async (req, res, next) => {});
+exports.feedback_delete_get = asyncHandler(async (req, res, next) => { });
 
-exports.feedback_delete_post = asyncHandler(async (req, res, next) => {});
+exports.feedback_delete_post = asyncHandler(async (req, res, next) => { });
