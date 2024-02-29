e217fae516ea98ee4054404593ebd4cd6371f8ab
Chiptune <phamtienphat123@gmail.com>
2024-02-28 23:56:44 -0800

    Implement feedback create on POST method

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -16,10 +16,25 @@ exports.feedback_list = asyncHandler(async (req, res, next) => {
 exports.feedback_detail = asyncHandler(async (req, res, next) => { });
 
 exports.feedback_create_get = asyncHandler(async (req, res, next) => {
-    res.render("feedback_create_form", {title: "Feedback Create"})
+    res.render("feedback_create_form", { title: "Feedback Create" })
 });
 
-exports.feedback_create_post = asyncHandler(async (req, res, next) => { });
+exports.feedback_create_post = asyncHandler(async (req, res, next) => {
+    const currentDate = new Date();
+    console.log(req.body);
+    const NewFeedback = new FeedbackModel({
+        creationDate: currentDate,
+        name: req.body.name,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        feedbackType: req.body.feedback,
+        feedbackMessage: req.body.feedbackMessage,
+        attachment: req.body.attachment,
+    })
+
+    await NewFeedback.save();
+    res.redirect("/feedbacks");
+});
 
 exports.feedback_update_get = asyncHandler(async (req, res, next) => { });
 
