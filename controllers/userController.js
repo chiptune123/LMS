1ee7a2884933d5bb90e0153f1a383e5fca2026e3
Chiptune <phamtienphat123@gmail.com>
2024-03-16 13:43:56 -0700

    Implement controller for authorization

--- controllers/userController.js
+++ controllers/userController.js
@@ -236,3 +236,19 @@ exports.user_sign_out = asyncHandler(async (req, res, next) => {
     this.next(err);
   }
 });
+
+exports.allAccess = (req, res) => {
+  res.status(200).send("Public content.");
+}
+
+exports.userBoard = (req, res) => {
+  res.status(200).send("User content.");
+}
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send("Admin content.");
+}
+
+exports.moderatorBoard = (req, res) => {
+  res.status(200).send("Moderator content.");
+}
