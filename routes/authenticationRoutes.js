1b3576c565d2c24d5799d444fec9a56dadc06c9c
Chiptune <phamtienphat123@gmail.com>
2024-03-16 20:36:18 -0700

    Refractor authentication routes with express router

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -1,15 +1,18 @@
 const { verifySignUp } = require("../middlewares/verifySignUp.js");
 const controller = require("../controllers/userController");
+const express = require("express");
+const router = express.Router();
 
-module.exports = function (app) {
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-    next();
-  });
+router.use(function(req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Accept"
+  )
+  next();
+})
 
-  app.post("/api/auth/signup", [verifySignUp.checkDuplicateUsernameOrEmail], controller.user_create_post);
+router.post("/api/auth/signup", [verifySignUp.checkDuplicateUsernameOrEmail], controller.user_create_post);
 
-  app.post("/api/auth/signin", controller.user_sign_in);
+router.post("/api/auth/signin", controller.user_sign_in);
 
-  app.post("/api/auth/signout", controller.user_sign_out);
-};
+router.post("/api/auth/signout", controller.user_sign_out);
