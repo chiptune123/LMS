a58b19a122c17e297f908a8e965004aed5643383
Chiptune <phamtienphat123@gmail.com>
2024-03-16 21:08:20 -0700

    Add api test for each user

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,24 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { authJwt } = require("../middlewares/authJwt") 
 
 // Import User controller module
-const user_controller = require('../controllers/userController');
+const user_controller = require("../controllers/userController");
 
-router.get('/', user_controller.user_list);
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  next();
+});
+
+router.get("/", user_controller.user_list);
 
 router.get("/create", user_controller.user_create_get);
 
 router.post("/create", user_controller.user_create_post);
 
-router.get('/:id', user_controller.user_detail);
+router.get("/:id", user_controller.user_detail);
 
-router.get('/:id/delete', user_controller.user_delete_get);
+router.get("/:id/delete", user_controller.user_delete_get);
 
 router.post("/:id/delete", user_controller.user_delete_post);
 
-router.get('/:id/update', user_controller.user_update_get);
+router.get("/:id/update", user_controller.user_update_get);
 
 router.post("/:id/update", user_controller.user_update_post);
 
+router.get("/api/test/all", user_controller.allAccess);
+
+router.get("/api/test/user", [authJwt.verifyToken], user_controller.userBoard);
+
+router.get("/api/test/librarian", [authJwt.isLibrarian], user_controller.librarianBoard);
+
+router.get("/api/test/admin", [authJwt.isAdmin], user_controller.adminBoard);
 
 module.exports = router;
