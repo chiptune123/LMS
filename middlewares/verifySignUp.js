04f9525daa26a0fb37d0b51f0b87b1c65c7a9250
Chiptune <phamtienphat123@gmail.com>
2024-03-27 13:28:45 -0700

    Fix no longer accept call back error by using async

--- middlewares/verifySignUp.js
+++ middlewares/verifySignUp.js
@@ -3,33 +3,53 @@ const asyncHandler = require('express-async-handler');
 
 checkDuplicateUsernameOrEmail = asyncHandler(async (req, res, next) => {
     // User duplicate check
-    await User.findOne({ username: req.body.username }).exec((err, user) => {
-        if (err) {
-            res.status(500).send({ message: err });
-            return;
-        }
-
+    try {
+        const user = await User.findOne({ username: req.body.username }).exec();
         if (user) {
             res.status(400).send({ message: "Failed! Username is exist!" });
-            return;
         }
-    })
+    } catch (err) {
+        res.status(500).send({ message: err });
+    }
 
-
-    // Email duplicate check
-    await User.findOne({ email: req.body.email }).exec((err, user) => {
-        if (err) {
-            res.status(500).send({ message: err });
-            return;
-        }
-
-        if (user) {
+    try {
+        const email = await User.findOne({ email: req.body.email }).exec();
+        if (email) {
             res.status(400).send({ message: "Failed! Email is exist!" });
-            return;
         }
-    })
+    } catch (err) {
+        res.status(500), send({ message: err });
+    }
 
     next();
+
+    // await User.findOne({ username: req.body.username }).exec((err, user) => {
+    //     if (err) {
+    //         res.status(500).send({ message: err });
+    //         return;
+    //     }
+
+    //     if (user) {
+    //         res.status(400).send({ message: "Failed! Username is exist!" });
+    //         return;
+    //     }
+    // })
+
+
+//     // Email duplicate check
+//     await User.findOne({ email: req.body.email }).exec((err, user) => {
+//         if (err) {
+//             res.status(500).send({ message: err });
+//             return;
+//         }
+
+//         if (user) {
+//             res.status(400).send({ message: "Failed! Email is exist!" });
+//             return;
+//         }
+//     })
+
+
 });
 
 const verifySignUp = {
