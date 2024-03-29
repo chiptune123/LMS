81d079aada10f394264d85deeaab0df26395824a
Chiptune <phamtienphat123@gmail.com>
2024-03-28 22:53:23 -0700

    Fix type error

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -3,6 +3,7 @@ const User = require("../models/users");
 const asyncHandler = require("express-async-handler");
 const config = require("../config/auth.config.js");
 
+// This function verify JWT token and set the request object with userID field.
 verifyToken = asyncHandler(async (req, res, next) => {
     let token = req.session.token;
 
@@ -17,6 +18,7 @@ verifyToken = asyncHandler(async (req, res, next) => {
             })
         }
 
+        // set request object with userID properties.
         req.userID = decoded.id;
         next();
     })
@@ -24,19 +26,32 @@ verifyToken = asyncHandler(async (req, res, next) => {
 
 
 isAdmin = asyncHandler(async (req, res, next) => {
-    User.findById(req.userID).exec((err, user) => {
-        if (err) {
-            res.status(500).send({ message: err });
-            return;
-        }
+    try {
+        const user = await User.findById(req.userID).exec();
 
         if (user.role === "Admin") {
             next();
         } else {
             res.status(403).send({ message: "Require Admin Role!" });
-            return;
         }
-    })
+    } catch (err) {
+        res.status(500).send({message: err});
+        return;
+    }
+
+    // User.findById(req.userID).exec((err, user) => {
+    //     if (err) {
+    //         res.status(500).send({ message: err });
+    //         return;
+    //     }
+
+    //     if (user.role === "Admin") {
+    //         next();
+    //     } else {
+    //         res.status(403).send({ message: "Require Admin Role!" });
+    //         return;
+    //     }
+    // })
 })
 
 isLibrarian = asyncHandler(async (req, res, next) => {
@@ -46,10 +61,10 @@ isLibrarian = asyncHandler(async (req, res, next) => {
             return;
         }
 
-        if(user.role === "Librarian"){
+        if (user.role === "Librarian") {
             next();
         } else {
-            res.status(403).send({ message: "Require Librarian Role!"});
+            res.status(403).send({ message: "Require Librarian Role!" });
             return;
         }
     })
