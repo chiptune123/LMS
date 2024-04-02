e0937afd429c9182e7c3f9be6593d276c48fdd5c
Chiptune <phamtienphat123@gmail.com>
2024-04-02 14:30:18 -0700

    Modified routes endpoint by remove username url param and let jwt validate

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -6,18 +6,18 @@ const feedback_controller = require("../controllers/feedbackController");
 
 router.get("/", feedback_controller.feedback_list);
 
-router.get("/:username/:id", feedback_controller.feedback_detail);
+router.get("/:id", feedback_controller.feedback_detail);
 
 router.get("/create", feedback_controller.feedback_create_get);
 
 router.post("/create", feedback_controller.feedback_create_post);
 
-router.get("/:username/:id/update", feedback_controller.feedback_update_get);
+router.get("/:id/update", feedback_controller.feedback_update_get);
 
-router.post("/:username/:id/update", feedback_controller.feedback_update_post);
+router.post("/:id/update", feedback_controller.feedback_update_post);
 
-router.get("/:username/:id/delete", feedback_controller.feedback_delete_get);
+router.get("/:id/delete", feedback_controller.feedback_delete_get);
 
-router.post("/:username/:id/delete", feedback_controller.feedback_delete_post);
+router.post("/:id/delete", feedback_controller.feedback_delete_post);
 
 module.exports = router;
