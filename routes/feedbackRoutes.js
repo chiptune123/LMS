f438d072172239c70492cd62bee8e9d862bf35c5
Chiptune <phamtienphat123@gmail.com>
2024-04-06 00:27:34 -0700

    Refactor code of feedbackRoutes

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -7,19 +7,19 @@ const { verifySignUp } = require("../middlewares/verifySignUp");
 // Import feedback Controller 
 const feedback_controller = require("../controllers/feedbackController");
 
-router.get("/", [authJwt.verifyToken, authJwt.isAdminOrLibrarian] ,feedback_controller.feedback_list);
+router.get("/", [authJwt.verifyToken, authJwt.isAdminOrLibrarian], feedback_controller.feedback_list);
 
-router.get("/:id", [authJwt.verifyToken, authJwt.isAdminOrLibrarian] , feedback_controller.feedback_detail);
+router.get("/:id", [authJwt.verifyToken, authJwt.isAdminOrLibrarian], feedback_controller.feedback_detail);
 
 router.get("/create", feedback_controller.feedback_create_get);
 
 router.post("/create", feedback_controller.feedback_create_post);
 
-router.get("/:id/update", [authJwt.verifyToken, authJwt.isAdminOrLibrarian] , feedback_controller.feedback_update_get);
+router.get("/:id/update", [authJwt.verifyToken, authJwt.isAdminOrLibrarian], feedback_controller.feedback_update_get);
 
-router.post("/:id/update", [authJwt.verifyToken, authJwt.isAdminOrLibrarian] ,feedback_controller.feedback_update_post);
+router.post("/:id/update", [authJwt.verifyToken, authJwt.isAdminOrLibrarian], feedback_controller.feedback_update_post);
 
-router.get("/:id/delete", [authJwt.verifyToken, authJwt.isAdminOrLibrarian] , feedback_controller.feedback_delete_get);
+router.get("/:id/delete", [authJwt.verifyToken, authJwt.isAdminOrLibrarian], feedback_controller.feedback_delete_get);
 
 router.post("/:id/updateFeedbackStatus", [authJwt.verifyToken, authJwt.isAdminOrLibrarian], feedback_controller.feedback_update_status);
 
