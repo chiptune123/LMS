64941e395fbe8e0b5c560499f740b09657df044a
Chiptune <phamtienphat123@gmail.com>
2024-04-06 13:34:22 -0700

    Create basic route architecture of authorRoutes

---
+++ routes/authorRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const { authJwt } = require("../middlewares/authJwt");
+
+// Import Author controller
+const author_controller = require("../controllers/authorController");
+
+router.get("/", author_controller.author_list);
+
+router.get("/:id", author_controller.author_detail);
+
+router.get("/create", author_controller.author_create_get);
+
+router.post("/create", author_controller.author_create_post);
+
+router.get("/:id/update", author_controller.author_update_get);
+
+router.post("/:id/update", author_controller.author_update_post);
+
+router.get("/:id/delete", author_controller.author_delete_get);
+
+router.post("/:id/delete", author_controller.author_delete_post);
