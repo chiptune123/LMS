ea4c683b1da107569e85d33fbecdcfb1fd69ef8c
Chiptune <phamtienphat123@gmail.com>
2024-04-06 23:59:23 -0700

    Implement author detail of authorController

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -19,7 +19,17 @@ exports.author_list = asyncHandler(async (req, res, next) => {
 })
 
 exports.author_detail = asyncHandler(async (req, res, next) => {
+    try {
+        const authorDetail = await AuthorModel.findById(req.params.id).exec();
 
+        if(authorDetail) {
+            res.render("author_detail", { title: "Author Detail", author_detail: authorDetail});
+        } else {
+            res.status(404).render("errorPage", { message: "Author detail not found!", status: 404 });
+        }
+    } catch (err) {
+        res.render.status(500).render("errorPage", { message: err, status: 404});
+    }
 })
 
 exports.author_create_get = asyncHandler(async (req, res, next) => {
