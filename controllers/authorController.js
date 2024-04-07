25e6481d43ca467abe10ea25ada2ae9283588f5c
Chiptune <phamtienphat123@gmail.com>
2024-04-07 00:13:49 -0700

    Implement author create on GET method

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -22,18 +22,22 @@ exports.author_detail = asyncHandler(async (req, res, next) => {
     try {
         const authorDetail = await AuthorModel.findById(req.params.id).exec();
 
-        if(authorDetail) {
-            res.render("author_detail", { title: "Author Detail", author_detail: authorDetail});
+        if (authorDetail) {
+            res.render("author_detail", { title: "Author Detail", author_detail: authorDetail });
         } else {
             res.status(404).render("errorPage", { message: "Author detail not found!", status: 404 });
         }
     } catch (err) {
-        res.render.status(500).render("errorPage", { message: err, status: 404});
+        res.render.status(500).render("errorPage", { message: err, status: 404 });
     }
 });
 
 exports.author_create_get = asyncHandler(async (req, res, next) => {
-
+    try {
+        res.render("author_create_form", { title: "Author Create" });
+    } catch (err) {
+        res.status(500).render("errorPage", { message: err, status: 404 });
+    }
 });
 
 exports.author_create_post = asyncHandler(async (req, res, next) => {
