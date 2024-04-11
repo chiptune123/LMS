265f00ad7369a25251a9cf40e3cc3ff856711bd8
Chiptune <phamtienphat123@gmail.com>
2024-04-11 12:18:08 -0700

    Implement subject list controller

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -2,7 +2,15 @@ const asyncHandler = require("express-async-handler");
 const BookModel = require("../models/books");
 const SubjectModel = require("../models/subjects");
 
-exports.subject_list = asyncHandler((req, res, next) => {});
+exports.subject_list = asyncHandler((req, res, next) => {
+  try {
+    const allSubject = SubjectModel.find({});
+
+    res.render("subject_list", { all_subject: allSubject });
+  } catch (err) {
+    res.status(500).render("errorPage", { message: err, status: 500 });
+  }
+});
 
 exports.subject_detail = asyncHandler((req, res, next) => {});
 
@@ -12,7 +20,7 @@ exports.subject_create_post = asyncHandler((req, res, next) => {});
 
 exports.subject_update_get = asyncHandler((req, res, next) => {});
 
-exports.subject_update_post = asyncHandler((req, res, next) => {})
+exports.subject_update_post = asyncHandler((req, res, next) => {});
 
 exports.subject_delete_get = asyncHandler((req, res, next) => {});
 
