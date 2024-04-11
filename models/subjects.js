a3538f0f7a62ce8a01924c0fdd3c847d95e0a73e
Chiptune <phamtienphat123@gmail.com>
2024-04-11 12:57:10 -0700

    Add default value to delete reason

--- models/subjects.js
+++ models/subjects.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const SubjectSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   deleteStatus: { type: Boolean, required: true, default: false },
-  deleteReason: { type: String, maxLength: 500 },
+  deleteReason: { type: String, maxLength: 500, default: ""},
 });
 
 module.exports = mongoose.model("Subject", SubjectSchema);
