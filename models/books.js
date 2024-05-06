a1249263a7970fb63cf1b6ea8d0e994d29a4fda7
Chiptune <phamtienphat123@gmail.com>
2024-05-06 01:35:21 -0700

    Modified database schema

--- models/books.js
+++ models/books.js
@@ -9,26 +9,26 @@ const BookSchema = new Schema({
     description: { type: String, required: true, maxLength: 500 },
     publisher: {type: String, required: true, maxLength:100},
     publish_date: { type: Date, required: true },
-    page: {type: Number, required: true},
+    page_numbers: {type: Number, required: true},
     price: {type: Number, required: true},
     quantity: {type: Number, required: true},
-    last_lend_date: {type: Date},
+    //last_lend_date: {type: Date},
     ISBN_tenDigits: {type: String, required: true},
     ISBN_thirteenDigits: {type: String, required: true},
     coverPicturePath: {type: String, maxLength: 500},
     uniqueBarcode: {type: String, required: true, maxLength: 200},
-    comments: [
-        {
-            memberID: {type: Schema.Types.ObjectId, ref: "User", required: true},
-            textComment: {type: String, maxLength: 500},
-            deleteStatus: {type: Boolean, default: false},
-            deleteReason: {type: String, maxLength: 500},
-            editorID: {type: Schema.Types.ObjectId, ref: "User"},
-            rating: {type: Number},
-            date: {type: Date, required: true},
-            isEdited: {type: Boolean},
-        }
-    ]
+    // comments: [
+    //     {
+    //         memberID: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    //         textComment: {type: String, maxLength: 500},
+    //         deleteStatus: {type: Boolean, default: false},
+    //         deleteReason: {type: String, maxLength: 500},
+    //         editorID: {type: Schema.Types.ObjectId, ref: "User"},
+    //         rating: {type: Number},
+    //         date: {type: Date, required: true},
+    //         isEdited: {type: Boolean},
+    //     }
+    // ]
 })
 
 module.exports = mongoose.model("Book", BookSchema);
