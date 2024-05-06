88d7c6e966d00c775dffa7a72561eae0c87201cb
Chiptune <phamtienphat123@gmail.com>
2024-05-06 01:02:30 -0700

    Implement book list controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -5,7 +5,17 @@ const SubjectModel = require("../models/subjects");
 const asyncHandler = require("express-async-handler");
 
 exports.book_list = asyncHandler(async (req, res, next) => {
+    try {
+        const allBooks = await BookModel.find().sort({ title: 1 }).exec();
 
+        if (allBooks) {
+            res.render("book_list", { title: "Book List", book_list: allBooks });
+        } else {
+            res.status(404).render("errorPage", { message: "No Book Found", errorStatus: 404 });
+        }
+    } catch (err) {
+        res.status(500).render("errorPage", { message: err, errorStatus: 500 });
+    }
 });
 
 exports.book_detail = asyncHandler(async (req, res, next) => {
