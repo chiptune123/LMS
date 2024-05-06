98096285dff7222acde161efc10d462e3ac4cff6
Chiptune <phamtienphat123@gmail.com>
2024-05-06 00:47:02 -0700

    Implement basic structure of book controller

---
+++ controllers/bookController.js
@@ -0,0 +1,37 @@
+const BookModel = require("../models/books");
+const AuthorModel = require("../models/authors");
+const SubjectModel = require("../models/subjects");
+
+const asyncHandler = require("express-async-handler");
+
+exports.book_list = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.book_detail = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.book_create_get = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.book_create_post = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.book_update_get = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.book_update_post = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.book_delete_get = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.book_delete_post = asyncHandler(async (req, res, next) => {
+
+});
