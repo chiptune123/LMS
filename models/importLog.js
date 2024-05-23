061102510de41626155b6fc0f218b514f933c31d
Chiptune <phamtienphat123@gmail.com>
2024-05-23 13:03:45 -0700

    Fix typo error of importLog model

--- models/importLog.js
+++ models/importLog.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ImportLogSchema = new Schema({
-    managerID: {typeof: Schema.Types.ObjectId, ref: "User", required: true},
-    bookID: {typeof: Schema.Types.ObjectId, ref: "Book", require: true},
+    managerId: {typeof: Schema.Types.ObjectId, ref: "User", required: true},
+    bookId: {typeof: Schema.Types.ObjectId, ref: "Book", require: true},
     dateTaken: {typeof: Date, required: true},
     supplier: {typeof: String, required: true},
     quantity: {typeof: Number, required: true},
