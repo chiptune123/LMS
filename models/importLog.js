d20510752d6ccddb18accb60bd6c3c6692e8995c
Chiptune <phamtienphat123@gmail.com>
2024-05-23 13:35:06 -0700

    Add timestamp in import model

--- models/importLog.js
+++ models/importLog.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const ImportLogSchema = new Schema({
-    managerId: {typeof: Schema.Types.ObjectId, ref: "User", required: true},
-    bookId: {typeof: Schema.Types.ObjectId, ref: "Book", require: true},
-    dateTaken: {typeof: Date, required: true},
-    supplier: {typeof: String, required: true},
-    quantity: {typeof: Number, required: true},
-})
+const ImportLogSchema = new Schema(
+  {
+    managerId: { typeof: Schema.Types.ObjectId, ref: "User", required: true },
+    bookId: { typeof: Schema.Types.ObjectId, ref: "Book", require: true },
+    dateTaken: { typeof: Date, required: true },
+    supplier: { typeof: String, required: true },
+    quantity: { typeof: Number, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("ImportLog", ImportLogSchema);
