92ddeede6dfe8af60caa569679462c80ba5b1a32
Chiptune <phamtienphat123@gmail.com>
2024-05-25 02:38:35 -0700

    Add middleware to check user role before allow access

--- routes/importRoutes.js
+++ routes/importRoutes.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var authJwt = require("../middlewares/authJwt");
 
 const importLog_controller = require("../controllers/importLogController") ;
 
-router.get("/", importLog_controller.import_create_get);
+router.get("/", [authJwt.isAdmin], importLog_controller.import_list);
 
-router.post("/", importLog_controller.import_create_post);
+router.get("/create", [authJwt.isAdmin], importLog_controller.import_create_get);
+
+router.post("/create", [authJwt.isAdmin], importLog_controller.import_create_post);
 
 module.exports = router;
