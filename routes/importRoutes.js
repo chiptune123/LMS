643bd7b183cf882c6c30ea977a9bf222dc8e9ba6
Chiptune <phamtienphat123@gmail.com>
2024-06-02 23:53:16 -0700

    Remove import delete route on GET method

--- routes/importRoutes.js
+++ routes/importRoutes.js
@@ -3,11 +3,16 @@ var router = express.Router();
 var authJwt = require("../middlewares/authJwt");
 
 const importLog_controller = require("../controllers/importLogController") ;
+const importLog = require('../models/importLog');
 
 router.get("/", [authJwt.isAdmin], importLog_controller.import_list);
 
 router.get("/create", [authJwt.isAdmin], importLog_controller.import_create_get);
 
 router.post("/create", [authJwt.isAdmin], importLog_controller.import_create_post);
+
+//router.get("/:id/delete", authJwt.isAdmin, importLog_controller.import_delete_get);
+
+router.post("/:id/delete", authJwt.isAdmin, importLog_controller.import_delete_post);
 
 module.exports = router;
