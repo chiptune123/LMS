d1ad82826c9a32536c77cef91483794963c90ac1
Chiptune <phamtienphat123@gmail.com>
2024-06-04 15:01:28 -0700

    Implement order create on POST

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const OrderModel = require("../models/orders");
+const OrderItemModel = require("../models/orderItems");
 
 exports.order_detail = asyncHandler(async (req, res, next) => {
   try {
@@ -19,3 +20,27 @@ exports.order_detail = asyncHandler(async (req, res, next) => {
     res.status(500).render("errorPage", { message: err, errorStatus: 500 });
   }
 });
+
+
+exports.order_create_post = asyncHandler(async (req, res, next) => {
+  try{
+    const newOrder = new OrderModel ({
+      memberId: req.body.memberId,
+    })
+
+    await newOrder.save();
+
+    for(let i = 0; i < req.session.cart.length; i++) {
+      const newOrderItem = new OrderItemModel ({
+        orderId: newOrder.id,
+        bookId: req.session.cart.bookId[i],
+      })
+
+      await newOrderItem.save();
+    }
+
+    res.render("thankyou_page", {title: "Thank you for your order!"});
+  } catch (err){
+    res.status(500).render("errorPage", { message: err, errorStatus: 500 });
+  }
+})
