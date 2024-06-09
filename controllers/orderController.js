9c3c13ee0f5af364920a774402ba23461593607f
Chiptune <phamtienphat123@gmail.com>
2024-06-09 01:06:39 -0700

    Implement order list

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,15 +1,18 @@
 const asyncHandler = require("express-async-handler");
 const OrderModel = require("../models/orders");
 const OrderItemModel = require("../models/orderItems");
+const { options } = require("../routes/authenticationRoutes");
 
 exports.order_detail = asyncHandler(async (req, res, next) => {
   try {
-    const orderDetail = OrderModel.findById(req.params.id);
+    const orderDetail = await OrderModel.findById(req.params.id);
+    const orderItemsList = await OrderItemModel.find({id: req.params.id}).populate("bookId", sort({title: 1}));
 
     if (orderDetail) {
       res.render("order_detail", {
         title: "Order Detail",
         order_detail: orderDetail,
+        order_items_list: orderItemsList
       });
     } else {
       res
@@ -21,6 +24,18 @@ exports.order_detail = asyncHandler(async (req, res, next) => {
   }
 });
 
+exports.order_list = asyncHandler(async (req, res, next) => {
+  try{
+    const orderList = await OrderModel.find({memberId: req.session.tokenUserId}).sort({createAt: 1});
+
+    if(orderList) {
+      res.render("order_list", {title: "Order List", order_list: orderList});
+    }
+
+  } catch (err) {
+    res.status(500).render("errorPage", { message: err, errorStatus: 500 });
+  }
+})
 
 exports.order_create_post = asyncHandler(async (req, res, next) => {
   try{
@@ -30,6 +45,7 @@ exports.order_create_post = asyncHandler(async (req, res, next) => {
 
     await newOrder.save();
 
+    // Save each item to a seperate OrderItems table
     for(let i = 0; i < req.session.cart.length; i++) {
       const newOrderItem = new OrderItemModel ({
         orderId: newOrder.id,
