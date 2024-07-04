af0824d047d054e67a6144078083428dcfe05aa1
Chiptune <phamtienphat123@gmail.com>
2024-07-04 16:13:30 -0700

    Add a route to contact us page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.redirect("/books");
 });
 
+router.get('/contact', function(req, res, next) {
+  res.render("contact", {title: "Contact us"});
+});
+
 module.exports = router;
