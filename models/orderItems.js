07b003a11badb36876486312fbe172be234a5e1d
Chiptune <phamtienphat123@gmail.com>
2024-07-05 13:52:34 -0700

    Fix typo error in orderItems model

--- models/orderItems.js
+++ models/orderItems.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const OrderItemSchema = new Schema({
-    orderId: { typeof: Schema.Types.ObjectId, ref: "Order", required: true },
-    bookId: { typeof: Schema.Types.ObjectIdm, ref: "Book", required: true },
-    returnDeadline: { typeof: Date },
-    penaltyAmount: {typeof: Number, Default: 0},
-    penaltyStatus: {typeof: Boolean, default: 0},
-    penaltyAmount: {typeof: double, default: 0},
-    lendStatus: {typeof: Boolean, default: 0}
+    orderId: { type: Schema.Types.ObjectId, ref: "Order", required: true },
+    bookId: { type: Schema.Types.ObjectIdm, ref: "Book", required: true },
+    returnDeadline: { type: Date },
+    penaltyAmount: {type: Number, Default: 0},
+    penaltyStatus: {type: Boolean, default: 0},
+    penaltyAmount: {type: double, default: 0},
+    lendStatus: {type: Boolean, default: 0},
+    quantity: {type: Number, default: 1},
 })
 
 module.exports = mongoose.model('OrderItem', OrderItemSchema);
