cc69a631de50d6d5aac9d4aa5f3aa5ccee923610
Chiptune <phamtienphat123@gmail.com>
2024-07-06 13:26:10 -0700

    Add a routes to handle checkout process

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const authJwt = require("../middlewares/authJwt");
 
 const book_controller = require("../controllers/bookController");
+const order_controller = require("../controllers/orderController");
 
 router.get("/", book_controller.cart_detail_get);
+
+router.post("/checkout", [authJwt.verifyToken], order_controller.order_create_post);
 
 router.post("/add_to_cart", book_controller.add_cart_post);
 
