f4b3a5f51b82b5db4392ecbf73b10233722074ba
Chiptune <phamtienphat123@gmail.com>
2024-07-06 13:21:04 -0700

    Change urlencoded to true

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use( express.static('public'));
@@ -80,7 +80,7 @@ app.use('/authors', authorRouter);
 app.use('/subjects', subjectRouter);
 app.use('/books', bookRouter);
 app.use('/imports', authJwt.verifyToken, importRouter);
-app.use('/cart', cartRouter);
+app.use('/carts', cartRouter);
 app.use('/orders', orderRouter);
 
 
