19cc48820c36559bd5aa260a0bed866229343e66
Chiptune <phamtienphat123@gmail.com>
2024-07-09 14:16:51 -0700

    Remove admin route from index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const asyncHandler = require('express-async-handler');
+
+const authJwt = require('../middlewares/authJwt');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.redirect("/books");
 });
 
