2777b90a43332ac24c376bd9ded09547e5c60958
Chiptune <phamtienphat123@gmail.com>
2024-07-10 15:34:03 -0700

    Add a route for feedback management

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -5,6 +5,7 @@ const asyncHandler = require('express-async-handler');
 var authJwt = require("../middlewares/authJwt");
 const book_controller = require('../controllers/bookController');
 const author_controller = require("../controllers/authorController");
+const feedback_controller = require("../controllers/feedbackController");
 
 router.get('/dashboard', asyncHandler(async (req, res, next) => {
     res.render("dashboard", { title: "Dashhboard Page" });
@@ -13,6 +14,8 @@ router.get('/dashboard', asyncHandler(async (req, res, next) => {
 router.get('/dashboard/book_data_table', book_controller.book_list);
 
 router.get('/dashboard/author_data_table', author_controller.author_list);
+
+router.get('/dashboard/feedback_data_table', feedback_controller.feedback_list);
 
 module.exports = router;
 
