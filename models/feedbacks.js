7d5f1b9799ef1c746e5c0acc68aca99c7e3d3244
Chiptune <phamtienphat123@gmail.com>
2024-07-10 15:33:21 -0700

    Remove attachment field

--- models/feedbacks.js
+++ models/feedbacks.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const feedbackSchema = new Schema({
-    creationDate: {type: Date},
-    name: {type: String, required: true, maxLength: 100},
-    email: {type: String, required: true, maxLength: 100},
-    phoneNumber: {type: Number},
-    feedbackType: {type: String, required: true, enum:["Question", "Compliment", "Bug", "Content", "Suggestion", "Other"]},
-    feedbackStatus: {type: Boolean, default: false},
-    feedbackMessage: {type: String, required: true, maxLength: 1000},
-    attachment: {type: String, maxLength: 500}
-})
+const feedbackSchema = new Schema(
+    {
+        name: { type: String, required: true, maxLength: 100 },
+        email: { type: String, required: true, maxLength: 100 },
+        phoneNumber: { type: Number },
+        feedbackType: { type: String, required: true, enum: ["Question", "Compliment", "Bug", "Content", "Suggestion", "Other"] },
+        feedbackStatus: { type: Boolean, default: false },
+        feedbackMessage: { type: String, required: true, maxLength: 1000 },
+        //attachment: { type: String, maxLength: 500 }
+    },
+    { timestamps: true })
 
-module.exports = mongoose.model("feedback",feedbackSchema);
+module.exports = mongoose.model("feedback", feedbackSchema);
