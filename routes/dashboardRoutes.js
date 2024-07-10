33f9715ea35bee190f4b5d39a07ece9a86a81598
Chiptune <phamtienphat123@gmail.com>
2024-07-10 00:26:40 -0700

    Add author data table route

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -4,12 +4,15 @@ const asyncHandler = require('express-async-handler');
 
 var authJwt = require("../middlewares/authJwt");
 const book_controller = require('../controllers/bookController');
+const author_controller = require("../controllers/authorController");
 
 router.get('/dashboard', asyncHandler(async (req, res, next) => {
     res.render("dashboard", { title: "Dashhboard Page" });
 }));
 
 router.get('/dashboard/book_data_table', book_controller.book_list);
+
+router.get('/dashboard/author_data_table', author_controller.author_list);
 
 module.exports = router;
 
