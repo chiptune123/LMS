58d0f87754189772e14d3aa398aa03201d5b8015
Chiptune <phamtienphat123@gmail.com>
2024-07-11 13:30:35 -0700

    Add staff and member management routes

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -6,7 +6,7 @@ var authJwt = require("../middlewares/authJwt");
 const book_controller = require('../controllers/bookController');
 const author_controller = require("../controllers/authorController");
 const feedback_controller = require("../controllers/feedbackController");
-
+const user_controller = require("../controllers/userController");
 router.get('/dashboard', asyncHandler(async (req, res, next) => {
     res.render("dashboard", { title: "Dashhboard Page" });
 }));
@@ -16,6 +16,10 @@ router.get('/dashboard/book_data_table', book_controller.book_list);
 router.get('/dashboard/author_data_table', author_controller.author_list);
 
 router.get('/dashboard/feedback_data_table', feedback_controller.feedback_list);
+
+router.get('/dashboard/user_management/member', user_controller.user_list_by_member);
+
+router.get('/dashboard/user_management/staff', user_controller.user_list_by_staff);
 
 module.exports = router;
 
