f3eb8098ff18a483f8fd47424fdc86bd20f46334
Chiptune <phamtienphat123@gmail.com>
2024-07-12 00:36:25 -0700

    Check req.baseUrl and response appropriately

--- controllers/importLogController.js
+++ controllers/importLogController.js
@@ -4,8 +4,29 @@ const ImportLogModel = require("../models/importLog");
 
 const asyncHandler = require("express-async-handler");
 
+exports.import_list = asyncHandler(async (req, res, next) => {
+  try {
+    const [importLogList, bookList] = await Promise.all([
+      ImportLogModel.find({}).populate("managerId").populate("bookId").sort({ createdAt: 1 }).exec(),
+      BookModel.find({}).sort({ title: 1 }).exec()
+    ]);
+    
+    if (importLogList && bookList) {
+      if (req.baseUrl == "/admin") {
+        res.render("import_log_management", { title: "Import List", import_list: importLogList, book_list: bookList });
+        return;
+      }
+    } else {
+      res.render("errorPage", { message: "Import Logs not found", errorStatus: 404 });
+    }
+  } catch (err) {
+    res.status(500).render("errorPage", { message: err, errorStatus: 500 });
+  }
+})
+
 exports.import_create_get = asyncHandler(async (req, res, next) => {
   try {
+    // Query bookList for import
     const bookList = await BookModel.find({}).exec();
     res.render("import_create_form", {
       title: "Import create",
@@ -59,44 +80,29 @@ exports.import_create_post = asyncHandler(async (req, res, next) => {
   }
 });
 
-
-exports.import_list = asyncHandler(async (req, res, next) => {
-  try {
-    const importLogList = await ImportLogModel.find({}).populate("managerId").populate("bookId").sort({ createdAt: 1 }).exec();
-
-    if (importLogList) {
-      res.render("import_list", { title: "Import List", import_list: importLogList });
-    } else {
-      res.render("errorPage", {message: "Import Logs not found", errorStatus: 404});
-    }
-  } catch (err) {
-    res.status(500).render("errorPage", { message: err, errorStatus: 500 });
-  }
-})
-
 exports.import_delete_get = asyncHandler(async (req, res, next) => {
-  try{
+  try {
     const importDetail = await ImportLogModel.findById(req.params.id);
 
-    if(importDetail) {
-      res.render("import_delete_form", {title: "Import Delete", import_detail: importDetail});
+    if (importDetail) {
+      res.render("import_delete_form", { title: "Import Delete", import_detail: importDetail });
     }
-  } catch (err){
-    res.render("errorPage", {message: "Import Logs not found", errorStatus: 404});
+  } catch (err) {
+    res.render("errorPage", { message: "Import Logs not found", errorStatus: 404 });
   }
 })
 
-exports.import_delete_post = asyncHandler(async (req, res, next ) => {
-  try{
-      const importDetail = await ImportLogModel.findByIdAndDelete(req.params.id).exec();
-      
-      if(importDetail == null) {
-        res.status(404).render("errorPage", {message: "Import not found!", errorStatus: 404});
-      }
-      
-      res.redirect("/imports");
+exports.import_delete_post = asyncHandler(async (req, res, next) => {
+  try {
+    const importDetail = await ImportLogModel.findByIdAndDelete(req.params.id).exec();
+
+    if (importDetail == null) {
+      res.status(404).render("errorPage", { message: "Import not found!", errorStatus: 404 });
+    }
+
+    res.redirect("/imports");
 
   } catch (err) {
-    res.status(500).render("errorPage", {message: err, errorStatus: 500});
+    res.status(500).render("errorPage", { message: err, errorStatus: 500 });
   }
 })
