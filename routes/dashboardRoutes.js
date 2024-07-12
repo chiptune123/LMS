8e3b8cd71783da2b906eb372a53a7afd49f1efb4
Chiptune <phamtienphat123@gmail.com>
2024-07-11 20:40:01 -0700

    Add announcement controller route

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -7,6 +7,8 @@ const book_controller = require('../controllers/bookController');
 const author_controller = require("../controllers/authorController");
 const feedback_controller = require("../controllers/feedbackController");
 const user_controller = require("../controllers/userController");
+const announcement_controller = require("../controllers/announcementController");
+
 router.get('/dashboard', asyncHandler(async (req, res, next) => {
     res.render("dashboard", { title: "Dashhboard Page" });
 }));
@@ -20,6 +22,8 @@ router.get('/dashboard/feedback_data_table', feedback_controller.feedback_list);
 router.get('/dashboard/user_management/member', user_controller.user_list_by_member);
 
 router.get('/dashboard/user_management/staff', user_controller.user_list_by_staff);
+
+router.get('/dashboard/announcement_management', announcement_controller.announcement_list);
 
 module.exports = router;
 
