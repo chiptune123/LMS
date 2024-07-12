f8e62045e877d4a1e5e4bbeb54c7c68e732ed21e
Chiptune <phamtienphat123@gmail.com>
2024-07-11 20:40:44 -0700

    Add timestamp property

--- models/announcements.js
+++ models/announcements.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const AnnouncementSchema = new Schema({
-    creationDate: {type: Date},
-    announcementContent: {type: String, required: true},
-    writerID: {type: Schema.Types.ObjectId, ref: "User" }
-})
+const AnnouncementSchema = new Schema(
+    {
+        creationDate: { type: Date },
+        announcementContent: { type: String, required: true },
+        writerID: { type: Schema.Types.ObjectId, ref: "User" }
+    },
+    { timestamps: true });
 
 module.exports = mongoose.model("Announcement", AnnouncementSchema);
