88dddbea47b2fd82c2ec6acd9bc416eeac89d6b7
Chiptune <phamtienphat123@gmail.com>
2024-07-13 23:19:31 -0700

    Modifed name of orderId field

--- models/renewalRequests.js
+++ models/renewalRequests.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const RenewalRequestSchema = new Schema({
-    itemID: { typeof: Schema.Types.ObjectId, ref: "OrderItem", required: true },
-    requestUser: { typeof: Schema.Types.ObjectId, ref: "User", required: true },
-    librarianID: { typeof: Schema.Types.ObjectId, ref: "User" },
-    reason: { typeof: String, required: true, maxLength: 500 },
-    requestExtendDate: { typeof: Date, required: true, default: Date.now },
-    requestStatus: { typeof: String, required: true, enum: ["Processing", "Denided", "Accept"], default: "Processing" }
-})
+const RenewalRequestSchema = new Schema(
+    {
+        orderItemId: { typeof: Schema.Types.ObjectId, ref: "OrderItem", required: true },
+        requestUser: { typeof: Schema.Types.ObjectId, ref: "User", required: true },
+        librarianId: { typeof: Schema.Types.ObjectId, ref: "User", default: "" },
+        reason: { typeof: String, required: true, maxLength: 500 },
+        requestExtendDate: { typeof: Date, required: true, default: Date.now },
+        requestStatus: { typeof: String, required: true, enum: ["Processing", "Denided", "Accept"], default: "Processing" }
+    },
+    { timestamps: true });
 
 module.exports = mongoose.model("RenewalRequest", RenewalRequestSchema);
