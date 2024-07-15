14a6af385dbdecccc2865c5825ef271309fa56bb
Chiptune <phamtienphat123@gmail.com>
2024-07-14 18:42:57 -0700

    Change phone number and feedback status property to string

--- models/feedbacks.js
+++ models/feedbacks.js
@@ -6,9 +6,9 @@ const feedbackSchema = new Schema(
     {
         name: { type: String, required: true, maxLength: 100 },
         email: { type: String, required: true, maxLength: 100 },
-        phoneNumber: { type: Number },
+        phoneNumber: { type: String, default: "" },
         feedbackType: { type: String, required: true, enum: ["Question", "Compliment", "Bug", "Content", "Suggestion", "Other"] },
-        feedbackStatus: { type: Boolean, default: false },
+        feedbackStatus: { type: String, require: true, enum: ["Pending", "Completed"], default: "Pending" },
         feedbackMessage: { type: String, required: true, maxLength: 1000 },
         //attachment: { type: String, maxLength: 500 }
     },
