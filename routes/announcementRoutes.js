caecfc503f3855b99a75978a8fbc957602256104
Chiptune <phamtienphat123@gmail.com>
2024-07-15 15:17:22 -0700

    Refactoring announcemnt routes

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -5,19 +5,13 @@ var router = express.Router();
 const annoucement_controller = require('../controllers/announcementController');
 
 router.get("/", annoucement_controller.announcement_list);
-// Create announcement routes
-router.get("/:id/create", annoucement_controller.announcement_create_get);
 
-router.post("/:id/create", annoucement_controller.announcement_create_post);
+router.post("/create", annoucement_controller.announcement_create_post);
 // Announcement detail routes
-router.get("/:id", annoucement_controller.announcement_detail);
+router.get("/:announcementId", annoucement_controller.announcement_detail);
 // Announcement delete routes
-router.get("/:username/:id/delete", annoucement_controller.announcement_delete_get );
-
-router.post("/:username/:id/delete", annoucement_controller.announcement_delete_post);
+router.post("/:announcementId/delete", annoucement_controller.announcement_delete_post);
 // Announcememt update routes
-router.get("/:username/:id/update", annoucement_controller.announcement_update_get);
-
-router.post("/:username/:id/update", annoucement_controller.announcement_update_post);
+router.post("/:announcementId/update", annoucement_controller.announcement_update_post);
 
 module.exports = router;
