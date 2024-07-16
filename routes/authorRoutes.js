e35d0b4166b9d8666979a982da47be64b0842fde
Chiptune <phamtienphat123@gmail.com>
2024-07-16 00:53:47 -0700

    Remove unnecessary routes

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -7,22 +7,13 @@ const author_controller = require("../controllers/authorController");
 
 router.get("/", author_controller.author_list);
 // Specific the routes endpoint should be first because endpoint with parameter will take wrong information.
-router.get("/create", author_controller.author_create_get);
 
 router.post("/create", author_controller.author_create_post);
 
-router.get("/:id", author_controller.author_detail);
+router.get("/:authorId", author_controller.author_detail);
 
-// router.get("/create", author_controller.author_create_get);
+router.post("/:authorId/update", author_controller.author_update_post);
 
-// router.post("/create", author_controller.author_create_post);
-
-router.get("/:id/update", author_controller.author_update_get);
-
-router.post("/:id/update", author_controller.author_update_post);
-
-router.get("/:id/delete", author_controller.author_delete_get);
-
-router.post("/:id/delete", author_controller.author_delete_post);
+router.post("/:authorId/delete", author_controller.author_delete_post);
 
 module.exports = router;
