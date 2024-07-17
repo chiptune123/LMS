7daebf8d21fb0cb5e19018ed95791dc45a855936
Chiptune <phamtienphat123@gmail.com>
2024-07-16 22:01:45 -0700

    Remove book model properties

--- models/books.js
+++ models/books.js
@@ -5,14 +5,11 @@ const Schema = mongoose.Schema;
 const BookSchema = new Schema({
     title: { type: String, required: true, maxLength: 100 },
     author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
-    // subject: { type: Schema.Types.ObjectId, ref: "Subject", required: true },
     description: { type: String, required: true, maxLength: 1000 },
     publisher: {type: String, required: true, maxLength:100},
     publish_date: { type: Date, required: true },
     page_numbers: {type: Number, required: true},
-    price: {type: Number, required: true},
     quantity: {type: Number, required: true},
-    //last_lend_date: {type: Date},
     ISBN_tenDigits: {type: String, required: true},
     ISBN_thirteenDigits: {type: String, required: true},
     coverPicturePath: {type: String, maxLength: 500},
@@ -27,18 +24,7 @@ const BookSchema = new Schema({
         ],
         default: "Available",
       },
-    // comments: [
-    //     {
-    //         memberID: {type: Schema.Types.ObjectId, ref: "User", required: true},
-    //         textComment: {type: String, maxLength: 500},
-    //         deleteStatus: {type: Boolean, default: false},
-    //         deleteReason: {type: String, maxLength: 500},
-    //         editorID: {type: Schema.Types.ObjectId, ref: "User"},
-    //         rating: {type: Number},
-    //         date: {type: Date, required: true},
-    //         isEdited: {type: Boolean},
-    //     }
-    // ]
+
 })
 
 BookSchema.virtual('ratingIntegralPart').get(function() {
