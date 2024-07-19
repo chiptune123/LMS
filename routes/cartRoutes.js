18e67cf6efef70343adf5522c5f9f2990bef7aea
Chiptune <phamtienphat123@gmail.com>
2024-07-18 17:47:56 -0700

    Add some seperate routes in book routes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,13 +4,14 @@ const authJwt = require("../middlewares/authJwt");
 
 const book_controller = require("../controllers/bookController");
 const order_controller = require("../controllers/orderController");
+const cart_controller = require("../controllers/cartController");
 
-router.get("/", book_controller.cart_detail_get);
+router.get("/", cart_controller.cart_detail_get);
 
 router.post("/checkout", [authJwt.verifyToken], order_controller.order_create_post);
 
-router.post("/add_to_cart", book_controller.add_cart_post);
+router.post("/add_to_cart", cart_controller.add_cart_post);
 
-router.post("/:id/remove_from_cart", book_controller.remove_from_cart_post);
+router.post("/remove_from_cart", cart_controller.remove_from_cart_post);
 
 module.exports = router;
