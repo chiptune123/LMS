a34dfd7741a21f92b153b19b8f4d866ba5d4175d
Chiptune <phamtienphat123@gmail.com>
2024-07-19 14:42:23 -0700

    Add deleteStatus property

--- models/importLog.js
+++ models/importLog.js
@@ -6,9 +6,9 @@ const ImportLogSchema = new Schema(
   {
     managerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     bookId: { type: Schema.Types.ObjectId, ref: "Book", require: true },
-    //dateTaken: { type: Date, required: true },
     supplier: { type: String, required: true },
     quantity: { type: Number, required: true },
+    deleteStatus: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
