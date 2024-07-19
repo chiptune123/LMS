6154b5fc04be2e0d05ac3c49732663c4a45d4e9e
Chiptune <phamtienphat123@gmail.com>
2024-07-19 14:31:10 -0700

    Refactoring feedback controller

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -10,7 +10,7 @@ exports.feedback_list = asyncHandler(async (req, res, next) => {
 
     if (feedbackList) {
       if (req.baseUrl == "/admin") {
-        res.render("feedbackManagement", { title: "Feedback Collection", feedback_list: feedbackList });
+        res.render("feedback_management", { title: "Feedback Collection", feedback_list: feedbackList });
         return;
       }
       res.render("contact", {
@@ -25,26 +25,12 @@ exports.feedback_list = asyncHandler(async (req, res, next) => {
   }
 });
 
-exports.feedback_detail = asyncHandler(async (req, res, next) => {
-  const feedbackDetail = await FeedbackModel.findById(req.params.id).exec();
-
-  if (feedbackDetail) {
-    res.render("feedback_detail_form", {
-      title: "Feedback Detail",
-      feedback_detail: feedbackDetail,
-    });
-  } else {
-    res.render("errorPage", { status: 404, message: "Feedback not found!" })
-  }
-});
-
 exports.feedback_create_get = asyncHandler(async (req, res, next) => {
   res.render("contact", { title: "Feedback Create" });
 });
 
 exports.feedback_create_post = asyncHandler(async (req, res, next) => {
   const currentDate = new Date();
-  console.log(req.body);
   const NewFeedback = new FeedbackModel({
     creationDate: currentDate,
     name: req.body.name,
@@ -59,40 +45,19 @@ exports.feedback_create_post = asyncHandler(async (req, res, next) => {
   res.redirect("/");
 });
 
-exports.feedback_update_get = asyncHandler(async (req, res, next) => {
-  const feedbackDetail = FeedbackModel.findById(req.params.id);
-
-  if (!feedbackDetail) {
-    res.render("errorPage", { errorStatus: 404 });
-  } else {
-    res.render("feedback_update_form", {
-      title: "Feedback Update",
-      feedback: feedbackDetail,
-    });
-  }
-});
-
 exports.feedback_update_post = asyncHandler(async (req, res, next) => {
   const currentDate = new Date();
-
-  //const NewFeedback = await FeedbackModel.findOne(req.params.id);
-  FeedbackModel.updateOne(
-    { _id: req.params.id },
+  await FeedbackModel.updateOne(
+    { _id: req.params.feedbackId },
     {
       $set: {
-        creationDate: currentDate,
-        name: req.body.name,
-        email: req.body.email,
-        phoneNumber: req.body.phoneNumber,
-        feedbackType: req.body.feedback,
-        feedbackMessage: req.body.feedbackMessage,
         feedbackStatus: req.body.feedbackStatus,
-        attachment: req.body.attachment,
+        updatedAt: new Date(),
       },
     }
   );
 
-  res.redirect("/feedbacks");
+  res.redirect("/admin/dashboard/feedback_management");
 });
 
 exports.feedback_delete_get = asyncHandler(async (req, res, next) => {
@@ -107,7 +72,8 @@ exports.feedback_delete_get = asyncHandler(async (req, res, next) => {
 
 exports.feedback_delete_post = asyncHandler(async (req, res, next) => {
   try {
-    await FeedbackModel.findByIdAndDelete(req.params.id).exec();
+    await FeedbackModel.findByIdAndDelete(req.params.feedbackId).exec();
+    res.redirect("/admin/dashboard/feedback_management");
   } catch (err) {
     res.render("errorPage", { message: err, status: 404 });
   }
