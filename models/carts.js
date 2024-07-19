8358cedd97ace6aac5de4d7819624fdda4361f21
Chiptune <phamtienphat123@gmail.com>
2024-07-18 17:43:22 -0700

    Refactoring books property and remove unnecessary properties

--- models/carts.js
+++ models/carts.js
@@ -5,12 +5,16 @@ const Schema = mongoose.Schema;
 const CartSchema = new Schema(
     {
         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        books: [{
-            book: {type: mongoose.Schema.ObjectId, ref: "Book"},
-            quantity: {type: Number, default: 1}
-        }],
-        active: { type: Boolean, default: true },
-        modifiedOn: { type: Date, default: Date.now }
+        books: {
+            type: [{
+                bookId: { type: Schema.Types.ObjectId, ref: "Book" },
+                quantity: { type: Number, default: 1 }
+            }],
+            default: []
+        }
+
+        // active: { type: Boolean, default: true },
+        //modifiedOn: { type: Date, default: Date.now }
 
     },
     { timestamps: true }
