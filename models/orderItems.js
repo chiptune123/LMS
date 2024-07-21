94f711fe52aacfce756ca7e1055069bf01580ef7
Chiptune <phamtienphat123@gmail.com>
2024-07-20 18:35:53 -0700

    Remove uniqueBarcode property

--- models/orderItems.js
+++ models/orderItems.js
@@ -5,10 +5,8 @@ const Schema = mongoose.Schema;
 const OrderItemSchema = new Schema({
     orderId: { type: Schema.Types.ObjectId, ref: "Order", required: true },
     bookId: { type: Schema.Types.ObjectId, ref: "Book", required: true },
-    uniqueBarcode: {type: String, required: true, maxLength: 200},
     returnDeadline: { type: Date },
     penaltyAmount: {type: Number, Default: 0},
-    //penaltyStatus: {type: Boolean, default: 0},
     lendStatus: {
         type: String,
         enum: ["Borrowed", "Returned", "Overdue"],
