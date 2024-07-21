fcdb08f63b99eab79dff0a2c80d8b0c621d48ca6
Chiptune <phamtienphat123@gmail.com>
2024-07-20 18:36:59 -0700

    Refactoring order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,12 +5,12 @@ var authJwt = require("../middlewares/authJwt");
 const order_controller = require("../controllers/orderController");
 
 // Order list by user
-router.get("/", [authJwt.verifyToken], order_controller.order_list_by_user);
+router.get("/", order_controller.order_list_by_user);
 
-router.post("/checkout", authJwt.verifyToken, order_controller.order_create_post);
+router.post("/checkout", order_controller.order_create_post);
 
-router.get("/:id", order_controller.order_detail);
+router.get("/:orderId", order_controller.order_detail);
 
-
+router.post("/:orderId/delete", order_controller.order_delete_post);
 
 module.exports = router;
