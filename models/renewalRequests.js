8fb6f6763b4998694b22fb51cd2a59b6e96b409c
Chiptune <phamtienphat123@gmail.com>
2024-07-28 21:59:28 -0700

    Fix reference error due to not correct model name

--- models/renewalRequests.js
+++ models/renewalRequests.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const RenewalRequestSchema = new Schema(
     {
         orderItemId: { type: Schema.Types.ObjectId, ref: "OrderItem", required: true },
-        bookId: { type: Schema.Types.ObjectId, ref: "book", required: true },
+        bookId: { type: Schema.Types.ObjectId, ref: "Book", required: true },
         userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
         oldReturnDeadline: { type: Date, require: true },
         requestExtendDate: { type: Date, required: true },
